837233351af30124ef1650783c4358a2a08a5937
HenriqueCardosoh <112336473+HenriqueCardosoh@users.noreply.github.com>
2023-01-22 22:32:54 -0300

    up5

--- Tarefa01.py
+++ Tarefa01.py
@@ -2,37 +2,55 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Valores possiveis para o eixo x 
-# O primeiro valor do paretense representa o valor minimo do grafico
-# O segundo o valor maximo
-# O terceiro representa a definicao do grafico, sendo zero a maior definicao possivel
-x = np.arange(-np.pi,np.pi, 0.0005)
+#QUESTAO 1
+def quest1():
+    # Valores possiveis para o eixo x 
+    # O primeiro valor do paretense representa o valor minimo do grafico
+    # O segundo o valor maximo
+    # O terceiro representa a definicao do grafico, sendo zero a maior definicao possivel
+    x = np.arange(-np.pi,np.pi, 0.0005)
 
-# Parametros que diferenciam cada uma das funcoes: w, y e z
-m1 = 1
-m2 = 2
-m3 = 3
+    # Parametros que diferenciam cada uma das funcoes: w, y e z
+    m1 = 1
+    m2 = 2
+    m3 = 3
 
-# Valores que correspondem as funcoes que serao tracadas
-w = np.cos(m1*x)
-y = np.cos(m2*x)
-z = np.cos(m3*x)
-  
-# Tracando o grafico de tres funcoes e colocando as marcacoes que vao diferenciar cada uma delas, incluindo o formato das linhas
-plt.plot(x, w, 'k--', label='m = 1')
-plt.plot(x, y, 'k:', label='m = 2')
-plt.plot(x, z, 'k-.', label='m = 3')
+    # Valores que correspondem as funcoes que serao tracadas
+    w = np.cos(m1*x)
+    y = np.cos(m2*x)
+    z = np.cos(m3*x)
+    
+    # Tracando o grafico de tres funcoes e colocando as marcacoes que vao diferenciar cada uma delas, incluindo o formato das linhas
+    plt.plot(x, w, 'k--', label='m = 1')
+    plt.plot(x, y, 'k:', label='m = 2')
+    plt.plot(x, z, 'k-.', label='m = 3')
 
-# Nome do eixo x
-plt.xlabel('x')
-# Nome do eixo y
-plt.ylabel('y')
-  
-# Colocando um titulo no gráfico que sera gerado
-plt.title('Gráfico adimensional de y(t) = cos(m*t) para m = 1, 2 e 3')
+    # Nome do eixo x
+    plt.xlabel('x')
+    # Nome do eixo y
+    plt.ylabel('y')
+    
+    # Colocando um titulo no gráfico que sera gerado
+    plt.title('Gráfico adimensional de y(t) = cos(m*t) para m = 1, 2 e 3')
 
-# Criando uma legenda para o gráfico e posicionando ela no canto superior esquerdo
-plt.legend(loc='upper left')
-  
-# Mostrando o grafico do resultado final
-plt.show()
+    # Criando uma legenda para o gráfico e posicionando ela no canto superior esquerdo
+    plt.legend(loc='upper left')
+    
+    # Mostrando o grafico do resultado final
+    plt.show()
+
+#QUESTAO 2.1
+
+
+#QUESTAO 2.2
+
+
+#QUESTAO 2.3
+
+
+#Function main utilizada para unir o fluxo de dados da resolução das questões para o usuário.
+
+def main():
+    quest1()
+    
+main()
